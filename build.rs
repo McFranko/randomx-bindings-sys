4ebf0d4303329d889d344048f35aabade518fe29
Matt Smith <matt@offtopica.uk>
2020-03-17 15:33:41 +0000

    Initial commit

---
+++ build.rs
@@ -0,0 +1,29 @@
+extern crate bindgen;
+extern crate cmake;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // TODO: Env var for setting `-DARCH=native`.
+    let dst = cmake::Config::new("randomx")
+        .build_target("randomx")
+        .build();
+
+    println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo:rustc-link-lib=static=randomx");
+
+    // TODO: Some systems may use clang libc++, or something else.
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+
+    let bindings = bindgen::Builder::default()
+        .header("randomx/src/randomx.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Unable to write bindings");
+}
